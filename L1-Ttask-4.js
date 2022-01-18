afd7a6262e06e2f213e0805aeff50af560a63ebe
Sergej Khmara <sergej.khmara.01@privatbank.ua>
2022-01-18 10:16:09 +0200

    Initial commit

---
+++ L1-Ttask-4.js
@@ -0,0 +1,31 @@
+//Перепишите код с использованием одной конструкции `switch`:
+var a = 0;
+
+if (a == 0) {
+    console.log( 0 );
+}
+if (a == 1) {
+    console.log( 1 );
+}
+
+if (a == 2 || a == 3) {
+    console.log( '2,3' );
+}
+
+switch (a)
+{
+    case 0:
+        console.log('0');
+        break;
+    case 1:
+        console.log('1');
+        break;
+    case 2:
+        console.log( '2,3' );
+        break;
+    case 3:
+        console.log ( '2,3' );
+        break;
+    default:
+        console.log('unknown input data not 0,1,2,3 ');
+}
---
+++ L1-Ttask-6.js
@@ -0,0 +1,9 @@
+//Написать код который посчитает сумму всех элементов в масиве.
+var arr = [1,2,3,4];
+var summas=0, b = arr.length
+
+for (i = 0; i < b ; i++)
+{
+    summas+=arr[i];
+}
+console.log( 'summa elementov massiva = ' + summas);
